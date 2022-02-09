029716d74fb5bd7e1eb9339db1fdb790e9d48025
Pattern-Projects <pattern.projects.hub@gmail.com>
2022-02-09 16:33:45 +0000

    Add Path functionality

--- src/com/pattern_projects/Main.java
+++ src/com/pattern_projects/Main.java
@@ -1,7 +1,13 @@
 package com.pattern_projects;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -10,10 +16,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input directory to empty:");
         String location = scanner.nextLine();
-        var root = new File(location);
-        if (root.exists()) {
+        var root = Paths.get(location);
+        if (Files.exists(root)) {
 
-            if (root.isDirectory()) {
+            if (Files.isDirectory(root)) {
                 System.out.println("Delete everything here? (yes, y, no, n)");
                 System.out.println(location);
 
@@ -22,15 +28,19 @@ public class Main {
                 var choice = scanner.next();
                 if (choice.toLowerCase().equals("y") || choice.toLowerCase().equals("yes")) {
 
-                    fileDeleter(root);
+                    //As Path
+                    //fileDeleter(root);
+
+                    //As File
+                    fileDeleter(root.toFile());
+
                     System.out.println("Complete");
+
                 } else {
                     //Does not want to delete
                     System.out.println("Will not delete directory contents.");
                     System.out.println("Process aborted.");
                 }
-
-
             } else {
                 //Is not a directory
                 System.out.println("This is a file, not a directory.");
@@ -51,7 +61,23 @@ public class Main {
             if (file.isDirectory()) {
                 fileDeleter(file);
             }
+            System.out.println("Deleting: "+file);
             file.delete();
+        }
+    }
+
+    public static void fileDeleter(Path root) {
+        try (var paths = Files.walk(root)) {
+            paths.filter(p -> !p.equals(root)).sorted(Comparator.comparingInt(p -> -p.getNameCount())).forEach(path -> {
+                try {
+                    System.out.println("Deleting: "+path);
+                    Files.delete(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
